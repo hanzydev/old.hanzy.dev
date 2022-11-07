6894f316ecc2d3c4609cd83ae6d20cfe8ea2666f
deliever42 <emirhanbaltas34@gmail.com>
2022-11-07 18:34:59 +0300

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    type DocumentContext,
+    type DocumentInitialProps,
+} from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    {CssBaseline.flush()}
+                </>
+            ),
+        };
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <title>Deliever42</title>
+
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta name="theme-color" content="#000000" />
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta
+                        name="description"
+                        content="Hello, my name is Emirhan. I live in Istanbul/Turkey, I am interested in software."
+                    />
+
+                    <meta property="og:title" content="Deliever42" />
+                    <meta
+                        property="og:description"
+                        content="Hello, my name is Emirhan. I live in Istanbul/Turkey, I am interested in software."
+                    />
+                    <meta property="og:image" content="/deliever.png" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:site_name" content="Deliever42" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
---
+++ src/pages/api/lanyard.tsx
@@ -0,0 +1,127 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Status = 'online' | 'idle' | 'dnd' | 'offline';
+
+enum StatusText {
+    online = 'Online',
+    idle = 'Idle',
+    dnd = 'Do Not Disturb',
+    offline = 'Offline',
+}
+
+type CodeActivity = {};
+
+type LanyardDiscordUserData = {
+    username: string;
+    discriminator: string;
+    tag: string;
+    id: string;
+    avatar_url: string | null;
+    status_text: StatusText;
+    status: Status;
+};
+
+type LanyardDataResponse = {
+    code: string;
+    custom_keys: Record<string, any>;
+    discord_user: LanyardDiscordUserData;
+    code_activities: CodeActivity[];
+    ok: true;
+};
+
+type LanyardErrorResponse = {
+    error: string;
+    code: string;
+    ok: false;
+};
+
+type LanyardResponse = LanyardDataResponse | LanyardErrorResponse;
+
+function resolveVSCActivityImage(raw: string) {
+    const match = raw.match(
+        /https\/raw\.githubusercontent\.com\/LeonardSSH\/vscord\/main\/assets\/icons\/([\w]*)(.*)/,
+    );
+
+    if (match) {
+        return `https://raw.githubusercontent.com/LeonardSSH/vscord/main/assets/icons/${match[1]}${match[2]}`;
+    }
+
+    return null;
+}
+
+function resolveVSActivityImage(raw: string) {
+    const match = raw.match(/File [\w]*(.*)/);
+
+    if (match) {
+        return `https://raw.githubusercontent.com/LeonardSSH/vscord/main/assets/icons/${match[1].slice(
+            1,
+        )}.png`;
+    }
+
+    return null;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<LanyardResponse>) {
+    const data = await fetch(
+        `https://api.lanyard.rest/v1/users/${process.env.DISCORD_USER_ID}`,
+    ).then((res) => res.json());
+
+    if (!data.success) {
+        return res
+            .status(400)
+            .json({ ok: false, code: data.error.code, error: data.error.message });
+    }
+
+    const discordUser = data.data.discord_user;
+
+    const codeActivities = (data.data.activities as Record<string, any>[])
+        .filter(
+            (activity) =>
+                activity.type === 0 &&
+                activity.state &&
+                ['Code', 'Visual Studio'].includes(activity.name),
+        )
+        .map((activity) => {
+            return {
+                name: activity.name === 'Code' ? 'Visual Studio Code' : activity.name,
+                state:
+                    activity.name === 'Code'
+                        ? activity.state
+                        : `Working on ${activity.details.replace(/File /g, '')}`,
+                details: activity.name === 'Code' ? activity.details : `In ${activity.state}`,
+                startTimestamp: activity.timestamps.start,
+                assets: {
+                    image:
+                        activity.name === 'Code'
+                            ? resolveVSCActivityImage(activity.assets.large_image)
+                            : resolveVSActivityImage(activity.details),
+                    text:
+                        activity.name === 'Code'
+                            ? activity.assets.large_text ?? activity.assets.small_text
+                            : activity.assets.large_text
+                            ? `Editing a ${activity.assets.large_text} file`
+                            : activity.assets.small_text,
+                },
+            };
+        });
+
+    return res.status(200).json({
+        ok: true,
+        code: 'success',
+        custom_keys: data.data.kv,
+        discord_user: {
+            username: discordUser.username,
+            discriminator: discordUser.discriminator,
+            tag: `${discordUser.username}#${discordUser.discriminator}`,
+            id: discordUser.id,
+            avatar_url: discordUser.avatar
+                ? `https://cdn.discordapp.com/avatars/${discordUser.id}/${discordUser.avatar}.${
+                      discordUser.avatar.startsWith('a_') ? 'gif' : 'png'
+                  }`
+                : null,
+            status_text: StatusText[discordUser.status as Status],
+            status: discordUser.status as Status,
+        },
+        code_activities: codeActivities,
+    });
+}
