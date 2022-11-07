b4d8a07bd260f1709e98a848b530ffac56603187
deliever42 <emirhanbaltas34@gmail.com>
2022-11-07 23:14:43 +0300

    feat: fetch repos and lanyard data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import React from 'react';
+import type { Repository, LanyardResponse } from '@/types';
 
 export default function Home() {
+    const [lanyard, setLanyard] = React.useState<LanyardResponse | null>(null);
+    const [repositories, setRepositories] = React.useState<Repository[] | null>(null);
+
+    React.useEffect(() => {
+        fetch('/api/lanyard')
+            .then((res) => res.json())
+            .then((data) => setLanyard(data));
+
+        fetch('/api/repositories')
+            .then((res) => res.json())
+            .then((data) => setRepositories(data.repositories));
+    }, []);
+
     return <div></div>;
 }
