9116a6d1b9f1b4d2ff94ba4a322b6d9899315715
deliever42 <emirhanbaltas34@gmail.com>
2022-11-07 23:37:47 +0300

    add normalize.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import 'normalize.css/normalize.css';
+import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
-
-import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
