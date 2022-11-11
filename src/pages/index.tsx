fc0dc0fac4c82f21b50a8dcc71fd7632dc1028a7
deliever42 <emirhanbaltas34@gmail.com>
2022-11-11 16:27:06 +0300

    fixing bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const Profile = dynamic(() => import('@/components/Profile/Profile'));
-
 export default function Home() {
+    const Profile = dynamic(() => import('@/components/Profile/Profile'));
+
     return (
         <div className="flex flex-col items-center justify-center h-[100vh] w-full container">
             <Profile />
