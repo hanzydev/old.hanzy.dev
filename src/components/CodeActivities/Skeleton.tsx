c0a340e5dde4814377269cc99f50213f7650d766
deliever42 <emirhanbaltas34@gmail.com>
2022-11-11 23:21:14 +0300

    feat: skeleton for code activities

--- src/components/CodeActivities/Skeleton.tsx
+++ src/components/CodeActivities/Skeleton.tsx
@@ -1,18 +1,37 @@
 import { Spacer } from '@nextui-org/react';
+import { Grid, Card, Text } from '@nextui-org/react';
 
 export default function Skeleton() {
     return (
-        <div className="shadow rounded-md p-4 m-0 backdrop-blur-lg">
-            <div className="flex flex-col sm:flex-row items-center text-center sm:text-left space-x-0 sm:space-x-4 animate-pulse">
-                <div className="rounded-lg bg-[#2b2f31] h-[21vh] w-[21vh] sm:h-[17vh] sm:w-[17vh]"></div>
-                <div className="mt-6 sm:mt-0 space-y-2 sm:space-y-0 flex flex-col items-center sm:items-baseline">
-                    <div className="bg-[#2b2f31] rounded w-72 h-8 sm:w-64"></div>
-                    <Spacer y={1} />
-                    <div className="bg-[#2b2f31] rounded w-[25rem] h-4 sm:w-[100vh]"></div>
-                    <div className="bg-[#2b2f31] rounded w-[25rem] h-4 sm:hidden"></div>
-                    <div className="bg-[#2b2f31] rounded w-[10rem] h-4 sm:hidden"></div>
-                </div>
-            </div>
-        </div>
+        <Grid.Container gap={2} justify="center">
+            <Grid xs={12} sm={6} md={4} lg={3}>
+                <Card css={{ p: '$6', borderColor: '$primary' }}>
+                    <div className="animate-pulse">
+                        <Card.Header>
+                            <Grid.Container>
+                                <Grid xs={12}>
+                                    <div className="bg-[#2b2f31] rounded w-[10rem] h-5"></div>
+                                </Grid>
+                                <Spacer y={0.7} />
+                                <Grid xs={12}>
+                                    <div className="bg-[#2b2f31] rounded w-[7.5rem] h-3"></div>
+                                </Grid>{' '}
+                            </Grid.Container>
+                            <div className="bg-[#2b2f31] rounded w-[55px] h-[45px]"></div>
+                        </Card.Header>
+                        <Spacer y={0.35} />
+                        <Card.Body css={{ py: '$2' }}>
+                            <div className="bg-[#2b2f31] rounded w-[12.7rem] h-3"></div>
+                        </Card.Body>
+                        <Spacer y={0.35} />
+                        <Card.Footer>
+                            <Text span color="primary">
+                            <div className="bg-[#2b2f31] rounded w-[5rem] h-3"></div>
+                            </Text>
+                        </Card.Footer>
+                    </div>
+                </Card>
+            </Grid>
+        </Grid.Container>
     );
 }
