574fd6c94e7b4ac063d846c059858016e93db898
deliever42 <emirhanbaltas34@gmail.com>
2022-11-12 15:47:28 +0300

    update parallax animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import resolveLanyardData from '@/utils/resolveLanyardData';
 import type { LanyardData, Repository } from '@/types';
-import { Text } from '@nextui-org/react';
+import { Text, Spacer } from '@nextui-org/react';
 import { Parallax } from 'react-scroll-parallax';
 
 const Profile = dynamic(() => import('@/components/Profile/Profile'));
@@ -85,17 +85,21 @@ export default function Home() {
             <div className="flex flex-col items-center justify-center h-[100vh] w-full container">
                 <Profile avatarURL={data?.discord_user?.avatar_url} tag={data?.discord_user?.tag} />
             </div>
-            <Parallax speed={20}>
+            <Parallax speed={25}>
                 <div className="flex flex-col items-center justify-center h-[auto] w-full container">
                     <Text h1>Code Activities</Text>
                     <CodeActivities activities={data?.code_activities} />
                 </div>
-                <Parallax speed={20}>
-                    <div className="flex flex-col items-center justify-center h-[auto] w-full container">
-                        <Text h1>Repositories</Text>
-                        <Repositories repositories={repositories} />
-                    </div>
-                </Parallax>
+                <Spacer y={3} />
+                <div className="flex flex-col items-center justify-center h-[auto] w-full container">
+                    <Text h1>Repositories</Text>
+                    <Repositories repositories={repositories} />
+                </div>
+                <Spacer y={3} />
+                {/* <div className="flex flex-col items-center justify-center h-[auto] w-full container">
+                    <Text h1>Technologies I Use</Text>
+                    <Repositories repositories={repositories} />
+                </div> */}
             </Parallax>
         </div>
     );
