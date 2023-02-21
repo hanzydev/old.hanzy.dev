26fc829202fcd6c110eaa31d9ead4aab0c8d939f
deliever42 <emirhanbaltas34@gmail.com>
2023-02-21 23:34:29 +0300

    correct module name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image-edge', '@nuxt/robots'],
+    modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image-edge', '@nuxtjs/robots'],
     srcDir: 'src',
     ssr: false,
     css: ['normalize.css/normalize.css'],
