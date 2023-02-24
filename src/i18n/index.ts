b8b1a5496059f785245850304995491bba811db3
deliever42 <emirhanbaltas34@gmail.com>
2023-02-24 19:54:18 +0300

    .

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,21 +1,21 @@
 import { get } from 'lodash';
 
 import de from './locales/de.json';
-import en from './locales/en-US.json';
-import es from './locales/es-ES.json';
+import en from './locales/en.json';
+import es from './locales/es.json';
 import fr from './locales/fr.json';
 import it from './locales/it.json';
 import ru from './locales/ru.json';
 import tr from './locales/tr.json';
 import uk from './locales/uk.json';
 
-export type ILocale = 'de' | 'en-US' | 'es-ES' | 'fr' | 'it' | 'ru' | 'tr' | 'uk';
+export type ILocale = 'de' | 'en' | 'es' | 'fr' | 'it' | 'ru' | 'tr' | 'uk';
 
-export const localeMap = { de, 'en-US': en, 'es-ES': es, fr, it, ru, tr, uk };
+export const localeMap = { de, en, es, fr, it, ru, tr, uk };
 
 export const locales = Object.keys(localeMap) as ILocale[];
 
-export const defaultLocale = 'en-US';
+export const defaultLocale = 'en';
 
 export const processProperties = (localeData: string, props: Record<string, string> = {}) => {
     return Object.keys(props).reduce((acc, key) => acc.replace(`{${key}}`, props[key]), localeData);
